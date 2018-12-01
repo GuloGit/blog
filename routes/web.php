9311817f17f09ce5059cd00cde1adfba87f4858a
Ольга <ol.dolotova@yandex.ru>
2018-12-01 18:30:59 +0400

    Настроена система аутенфикации

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::group(["middleware"=>"auth"], function(){
+    Route::get('/admin',"Admin\AdminController@index")->name("admin");
+  });
+
+// Authentication Routes...
+Route::get('admin/login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('admin/login', 'Auth\LoginController@login');
+Route::get('admin/logout', 'Auth\LoginController@logout')->name('logout');
+
+// Password Reset Routes...
+Route::get('admin/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('admin/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('admin/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('admin/password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+Route::get('/', 'HomeController@index')->name('home');
