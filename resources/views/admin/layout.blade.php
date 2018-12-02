3f2d261ac3d9e2676e04995aa1a4b8920163cf9e
Ольга <ol.dolotova@yandex.ru>
2018-12-02 16:02:31 +0400

    Создан главный шаблон админ панели

---
+++ resources/views/admin/layout.blade.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>@yield("title")</title>
+
+    @section("style")
+    <style>
+        html {
+            position: relative;
+            min-height: 100%;
+        }
+        body{
+            background-image: url("fon.jpg");
+            background-color: #e3e3e3;
+        }
+
+        footer {
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            height: 40px;
+            }
+    </style>
+    @show
+
+</head>
+<body>
+    <div class="container">
+        <nav class="navbar navbar-light " style="background-color: #e3e3e3;">
+            <div class="btn-group" role="group">
+                <a  href="#" class="btn btn-outline-primary btn-lg ">Категории</a>
+                <a  href="#" class="btn btn-outline-primary btn-lg">Посты</a>
+                <a  href="#" class="btn btn-outline-primary btn-lg">Найти пост</a>
+            </div>
+            <button type="button" class="btn btn-outline-secondary btn-lg">ВЫХОД</button>
+        </nav>
+        <h1 class="mb-3">@yield("title")</h1>
+        @yield("content")
+        <footer class=" row-fluid">
+            @section("footer")
+            &copy; Наша супер админ панель
+            @show
+        </footer>
+    </div>
+    @stack("scripts")
+</body>
+</html>
